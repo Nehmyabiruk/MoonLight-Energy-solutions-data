af7ac2bf62458aceddc7d98e1203f7a07663580c
Nehmya biruk <164040220+Nehmyabiruk@users.noreply.github.com>
2026-08-18 13:06:30 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,10 +5,10 @@ from src.visualization import plot_time_series, compare_countries_hist
 if __name__ == "__main__":
     print("🌙 MoonLight Energy Solutions - Starting Analysis...\n")
     
-    data = load_all_data() rf jrekghrekj.
+    data = load_all_data()
     
     for country, df in data.items():
-        basic_info(df, country) bdtsvkhudc
+        basic_info(df, country)
     
     compare_countries_hist(data, column="GHI")
     print("\n✅ Analysis completed successfully!")
