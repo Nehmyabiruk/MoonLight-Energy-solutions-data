ca143a342e1be5579cf0cf70910eaae5dce3cb7b
Nehmya biruk <164040220+Nehmyabiruk@users.noreply.github.com>
2026-08-20 11:19:21 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from src.eda_utils import basic_info, check_missing_percentage
 from src.visualization import plot_time_series, compare_countries_hist
 
 if __name__ == "__main__":
-    print("🌙 MoonLight Energy Solutions - Starting Analysis...\n") yfgbuggh
+    print("🌙 MoonLight Energy Solutions - Starting Analysis...\n")
     
     data = load_all_data()
     
@@ -11,4 +11,4 @@ if __name__ == "__main__":
         basic_info(df, country)
     
     compare_countries_hist(data, column="GHI")
-    print("\n✅ Analysis completed successfully!")
+    print("\n✅ Analysis completed successfully!"
